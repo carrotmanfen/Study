bfe9c96ba313da2c37120cc0b3efb53d0707626b
carrotmanfen <kudsun.khamjamnun@mail.kmutt.ac.th>
2023-01-01 23:38:26 +0700

    finish

--- src/components/Client.jsx
+++ src/components/Client.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { clients } from '../constants'
+import styles from '../style'
 
 const Client = () => {
   return (
-    <div>Client</div>
+    <section className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full `}>
+        {clients.map((client)=>(
+          <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[220px] min-w-[120px] lg:max-h-[40px] `}>
+            <img src={client.logo} alt="client" className='sm:w-[192px] w-[100px] object-contain hover:w-[120px] sm:hover:w-[220px]'/>
+          </div>
+        ))}
+      </div>
+    </section>
   )
 }
 
